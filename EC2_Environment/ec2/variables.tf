cc14ddbd319e5ffb00c5dced6fd381f7c921345c
removedporn <86824510+removedporn@users.noreply.github.com>
2023-07-13 18:14:41 +0800

    Delete EC2_Environment directory

--- EC2_Environment/ec2/variables.tf
+++
@@ -1,4 +0,0 @@
-variable private_sg {}
-variable public_sg {}
-variable private_subnet {}
-variable public_subnet {}
--- EC2_Environment/network/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "vpc_id" {}
-variable "public_subnet" {}
-variable "private_subnet" {}
